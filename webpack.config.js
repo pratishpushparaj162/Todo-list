8a30564d7d43737e6106a0b2101aea50ed67b981
pratishpushparaj162 <pratish.pushparaj16@gmail.com>
2025-10-01 01:07:01 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       exclude: ['node_modules', 'dist/bundle.js'],
       loader: 'babel-loader',
       query: {
-        presets: ['es2015','react']
+      presets: ['es2015','react']
       }
     }]
   }
